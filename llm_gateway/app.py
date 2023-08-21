bcab042d97506bb77dc4242c4bc5b22547511297
Edward Qian <edward.qian@grafana.com>
2023-08-21 14:35:58 -0400

    added tracing and log bits

--- llm_gateway/app.py
+++ llm_gateway/app.py
@@ -15,12 +15,19 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import loguru
+
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi_route_logger_middleware import RouteLoggerMiddleware
 from starlette_exporter import PrometheusMiddleware, handle_metrics
 
 from llm_gateway.routers import cohere_api, openai_api
+from llm_gateway.utils import setup_tracing
+from llm_gateway.log import add_trace_id, init_logging
+
+
+APP_NAME = "llm_gateway"
 
 app = FastAPI()
 app.title = "LLM Proxy"
@@ -36,6 +43,8 @@ app.mount("/api", api, name="api")
 # Allow Front-end Origin in local development
 origins = ["http://localhost:3000"]
 
+
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
@@ -43,7 +52,8 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-app.add_middleware(RouteLoggerMiddleware)
+logger = loguru.logger.patch(add_trace_id)
+app.add_middleware(RouteLoggerMiddleware, logger=logger)
 app.add_middleware(
     PrometheusMiddleware,
     app_name="llm_gateway",
@@ -52,6 +62,8 @@ app.add_middleware(
     buckets=[0.01, 0.025, 0.05, 0.1, 0.25, 0.5, 1.0, 2.5, 5.0, 10.0, 30.0, 60.0, 120.0],
 )
 app.add_route("/metrics", handle_metrics)
+init_logging()
+setup_tracing(app, APP_NAME)
 
 
 @api.get("/healthcheck")
--- llm_gateway/utils.py
+++ llm_gateway/utils.py
@@ -1,5 +1,18 @@
 import logging
 
+from opentelemetry import trace
+from opentelemetry.exporter.jaeger import thrift
+from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
+from opentelemetry.instrumentation.logging import LoggingInstrumentor
+from opentelemetry.propagate import set_global_textmap
+from opentelemetry.propagators.jaeger import JaegerPropagator
+from opentelemetry.sdk.resources import SERVICE_NAME, Resource
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+from opentelemetry.trace import get_current_span
+from opentelemetry.trace.span import format_trace_id
+from starlette.types import ASGIApp
+
 level = logging.INFO
 logging.basicConfig(level=level)
 logger = logging.getLogger(__name__)
@@ -32,3 +45,30 @@ def max_retries(times: int, exceptions: tuple = (Exception)):
         return newfn
 
     return decorator
+
+
+def setup_tracing(app: ASGIApp, app_name: str, log_correlation: bool = True) -> None:
+    # Setting OpenTelemetry
+    # set the service name to show in traces
+    set_global_textmap(JaegerPropagator())
+    tracer_provider = TracerProvider(resource=Resource.create({SERVICE_NAME: app_name}))
+    trace.set_tracer_provider(tracer_provider)
+    
+    tracer_provider.add_span_processor(
+        BatchSpanProcessor(thrift.JaegerExporter())
+    )
+
+
+    if log_correlation:
+        LoggingInstrumentor().instrument(set_logging_format=True)
+
+    FastAPIInstrumentor.instrument_app(app, tracer_provider=tracer_provider)
+
+
+def add_trace_id(record) -> None:
+    sp = get_current_span()
+    if sp is None:
+        return
+
+    trace_id = sp.get_span_context().trace_id
+    record["extra"].update(trace_id=format_trace_id(trace_id))
